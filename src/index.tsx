3419b6f771f8c0302596f36e6bb9a7890989b0e5
VolumeData21 <volumedata21@gmail.com>
2026-01-19 20:42:17 -0500

    Fix index.ts

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
-}
-
-const root = ReactDOM.createRoot(rootElement);
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import { ToastProvider } from './components/ToastContext'; // Import the new file
+import { ToastProvider } from './components/ToastContext';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
@@ -21,7 +11,7 @@ if (!rootElement) {
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
-    <ToastProvider> 
+    <ToastProvider>
       <App />
     </ToastProvider>
   </React.StrictMode>
